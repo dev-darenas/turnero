ff6ee8c81a1e30d68d528bdd72b7af7c83877b25
albor <albor@PT-INGRY>
2019-04-11 20:30:47 -0500

    listModulo

---
+++ src/java/Controller/RegistroMod.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lib.ConsultasModulo;
+
+/**
+ *
+ * @author albor
+ */
+@WebServlet(name = "RegistroMod", urlPatterns = {"/RegistroMod"})
+public class RegistroMod extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+             
+        try {
+            int cod_modulo=Integer.parseInt(request.getParameter("cod"));
+            //CAPTURAR CAMPOS
+             String nombre=request.getParameter("nom");
+             String descripcion=request.getParameter("des");
+            ConsultasModulo co=new ConsultasModulo();
+            
+            if(co.registrar(cod_modulo, nombre,descripcion)){
+                request.getRequestDispatcher("vistas/modulo/GuardadoExitoso.jsp")
+                                         .forward(request,response);
+            }else{
+                request.getRequestDispatcher("vistas/modulo/crear.jsp")
+                                            .forward(request,response);
+            }
+        } finally{
+            out.close();
+        }
+    
+}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+    
